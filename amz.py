e7728c393da47952eb2d39d66edfd41e33ebbe02
zbz <zbz26678325@163.com>
2017-09-11 09:53:49 +0800

    pc

--- amz.py
+++ amz.py
@@ -6,13 +6,15 @@ import pymysql
 import time
 from bs4 import BeautifulSoup
 from multiprocessing.dummy import Pool
-from util import my_session
+from util import my_session,USER_AGENTS
+import random
 mysql_config = {
     "host":"59ae085c00753.gz.cdb.myqcloud.com",
     "user":"root",
     "password":"%yms%2017",
     "db":"yms_erp_dev",
-    "charset":"utf8",
+    #"db":"yms_test",
+    "charset":"utf8mb4",
     "port":5902,
 }
 # headers_list = {
@@ -43,22 +45,26 @@ def md5(data):
     m2.update(data)
     return m2.hexdigest()
 
-
+def get_asin_sid():
+    cursor.execute("select distinct asin1,sid from mws_product_online")
+    results = cursor.fetchall()
+    conn.commit()
+    return results
 def trans_format(time_string, from_format, to_format='%Y.%m.%d %H:%M:%S'):
     time_struct = time.strptime(time_string, from_format)
     times = time.strftime(to_format, time_struct)
     return times
 
 def my_get(url,proxies=None):
-    while 1:
-        try:
-            res = s.get(url)#,headers = headers,proxies=proxies,timeout=2)
-            break
-        except:
-            continue
+    s.headers.update({"User-Agent":random.choice(USER_AGENTS)})
+    try:
+        res = s.get(url)#,headers = headers,proxies=proxies,timeout=2)
+    except Exception as e:
+        print(e)
     return res
 
 def my_post(url,data,proxies=None):
+    s.headers.update({"User-Agent": random.choice(USER_AGENTS)})
     while 1:
         try:
             res = s.post(url,data=data,timeout=5)#proxies = proxies,headers=headers)
@@ -68,7 +74,7 @@ def my_post(url,data,proxies=None):
     return res
 #获取当前页数评论
 def get_xhr(page_num,asin):#,proxies,header):
-    request_data = "sortBy=recent&reviewerType=all_reviews&formatType=&filterByStar=&pageNumber={}&filterByKeyword=&shouldAppend=undefined&deviceType=desktop&reftag=cm_cr_arp_d_paging_btm_2&pageSize=50&asin={}&scope=reviewsAjax0".format(page_num,asin)
+    request_data = "sortBy=recent&reviewerType=all_reviews&formatType=current_format&filterByStar=&pageNumber={}&filterByKeyword=&shouldAppend=undefined&deviceType=desktop&reftag=cm_cr_arp_d_paging_btm_2&pageSize=50&asin={}&scope=reviewsAjax0".format(page_num,asin)
     XHR_res = my_post(url='https://www.amazon.com/ss/customer-reviews/ajax/reviews/get/ref=cm_cr_getr_d_paging_btm_next_5', data=request_data)#,proxies= proxies,headers=header)
     return XHR_res
 
@@ -94,12 +100,14 @@ def parse(ever_page_html):
     soup_result["last_content"] = soup.findAll("span", attrs={'data-hook': "review-body"})[0].text
     is_vp = soup.findAll("span", attrs={'data-hook': "avp-badge"})
     soup_result["is_vp"] = 1 if is_vp else 0
+    current_format = soup.findAll("a", attrs={'data-hook': "format-strip"})
+    soup_result["current_format"] = current_format[0].text if current_format else ''
     return soup_result
 
 
 #评论入库
 def my_db(data):
-    sql = 'INSERT INTO amz_review_copy (sid, asin, review_id, last_star, last_title,last_content,review_md5,author_id,author,review_date,is_vp,status,update_time,create_time,crawl_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+    sql = 'INSERT INTO amz_review (sid, asin, review_id, last_star, last_title,last_content,review_md5,author_id,author,review_date,is_vp,status,update_time,create_time,crawl_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
     try:
         cursor.execute(sql, data)
         conn.commit()
@@ -109,56 +117,71 @@ def my_db(data):
 
 
 #迭代入库
-def get_xhr_parse(html,asin,sid,response_list):
-    # now =time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+def deal_data(html,asin,sid):
+    now =time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
     html_list = html.split("&&&")
     for x in html_list[3:-3]:
         ever_page_html = json.loads(x)[2:]
-        response_list.append(ever_page_html)
+        soup_result = parse(ever_page_html=ever_page_html[0])
+        data = [sid, asin, soup_result["review_id"], soup_result["last_star"], soup_result["last_title"],
+                soup_result["last_content"], md5(soup_result["last_title"] + soup_result["last_content"]),
+                soup_result["author_id"], soup_result["author"], soup_result["review_date"], soup_result["is_vp"], 0,
+                int(time.time()), int(time.time()), now]
+        my_db(data)
         # soup_result = parse(ever_page_html=ever_page_html[0])
         # result_list.append([sid, asin, soup_result["review_id"], soup_result["last_star"], soup_result["last_title"], soup_result["last_content"], md5(soup_result["last_title"] + soup_result["last_content"]), soup_result["author_id"], soup_result["author"], soup_result["review_date"], soup_result["is_vp"], 0, int(time.time()), int(time.time()), now])
 def main_handler(each_rev,asin,sid,response_list):
     print("当前页：", each_rev)
     res = get_xhr(each_rev, asin)
-    get_xhr_parse(res.text,asin,sid,response_list)
+    #get_xhr_parse(res.text,asin,sid,response_list)
+    response_list.append(res)
     print("完成页：",each_rev)
 # 多线程 处理
-def
 def main(asin,sid):
+    print(asin ,sid)
     response_list = []
     start_url = 'https://www.amazon.com/product-reviews/{}?reviewerType=all_reviews&sortBy=recent'.format(asin)
     while 1:
         start_res = my_get(start_url)
+        if start_res.status_code == 404:
+            now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+            data = [sid, asin, "", "", "", "","","","","", "",0, int(time.time()), int(time.time()), now]
+            my_db(data)
+            return
+        elif start_res.status_code != 200:
+            continue
         try:
             review_num = rev_num(start_res.text)
             break
         except:
+            #print(start_res.text)
             pass
-    tasks = [(x,asin,sid,response_list)for x in range(1,review_num // 50 + 2)]
-    thread_num = len(tasks) if len(tasks)<20 else 20
-    pool = Pool(thread_num)
-    #tasks = [(x, asin) for x in range(1,20)]
-    pool.starmap(main_handler, tasks)
-    pool.close()
-    pool.join()
-    for respon in response_list:
-        data = []
-        now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        soup_result = parse(ever_page_html=respon[0])
-        data = [sid, asin, soup_result["review_id"], soup_result["last_star"], soup_result["last_title"], soup_result["last_content"], md5(soup_result["last_title"] + soup_result["last_content"]), soup_result["author_id"], soup_result["author"], soup_result["review_date"], soup_result["is_vp"], 0, int(time.time()), int(time.time()), now]
-        my_db(data)
+    print("共",review_num//50+2,"页")
 
-    # for x in result_list:
-    #     my_db(x)
-def get_asin_sid():
-    cursor.execute("select asin1,sid from mws_product_online")
-    results = cursor.fetchall()
-    conn.commit()
-    return results
+
+    if review_num == 1 :
+        main_handler(review_num,asin,sid,response_list)
+    else:
+        tasks = [(x,asin,sid,response_list)for x in range(1,review_num // 50 + 2)]
+        thread_num = len(tasks) if len(tasks)<30 else 30
+        pool = Pool(thread_num)
+        #tasks = [(x, asin) for x in range(1,20)]
+        pool.starmap(main_handler, tasks)
+        pool.close()
+        pool.join()
+
+
+
+    for respon in response_list:
+        deal_data(respon.text,asin,sid)
+        # now = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        # soup_result = parse(ever_page_html=respon[0])
+        # data = [sid, asin, soup_result["review_id"], soup_result["last_star"], soup_result["last_title"], soup_result["last_content"], md5(soup_result["last_title"] + soup_result["last_content"]), soup_result["author_id"], soup_result["author"], soup_result["review_date"], soup_result["is_vp"], 0, int(time.time()), int(time.time()), now]
+        # my_db(data)
+
 
 if __name__ == '__main__':
     asins_sids = get_asin_sid()
-    # #main(start_url,asin)
     for asin,sid in asins_sids:
         main(asin,sid)
-    #print(int(time.time()))
+    #main("B01D4FP6MY",1)
